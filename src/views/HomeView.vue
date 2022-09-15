7ac9254a4dce409c97f35ab29de648ebb0ae8baf
p0pl3 <m.poplavskiy04@gmail.com>
2022-09-16 00:20:13 +0300

    FEATURE: add information banners

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <template>
   <UpButtonPage/>
   <SliderSale/>
+  <InfoBanner/>
   <SliderProduct/>
 </template>
 <script>
 import UpButtonPage from "@/components/UpButtonPage";
 import SliderSale from "@/components/SliderSale";
 import SliderProduct from "@/components/SliderProduct";
+import InfoBanner from "@/components/InfoBanner";
 
 export default {
   name: 'HomeView',
-  components: {SliderProduct, SliderSale, UpButtonPage},
+  components: {SliderProduct, SliderSale, UpButtonPage, InfoBanner},
   data() {
     return {
 
